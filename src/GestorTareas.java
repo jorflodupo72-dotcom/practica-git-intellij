bb61bfb6045dc45a54c37e288613145449ff2a80
JorgeDuran72 <jorge.duran16@educa.madrid.org>
2026-03-19 14:14:16 +0100

    Añadida validación al agregar tareas

--- src/GestorTareas.java
+++ src/GestorTareas.java
@@ -5,6 +5,10 @@ public class GestorTareas {
     private List<Tarea> tareas = new ArrayList<>();
 
     public void agregarTarea(Tarea tarea) {
+        if (tarea == null) {
+            System.out.println("No se puede agregar una tarea nula.");
+            return;
+        }
         tareas.add(tarea);
     }
 
